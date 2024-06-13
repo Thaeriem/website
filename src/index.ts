8a062f2f3137d18b66924cb29f768c383d2181c8
Thaeriem <kauly.cs@gmail.com>
2024-06-13 11:26:29 -0700

    refactoring code with dummy objects

--- src/index.ts
+++ src/index.ts
@@ -30,9 +30,9 @@ let islandModel: THREE.Object3D,
     cloudModel: THREE.Object3D, 
     boatModel: THREE.Object3D
 let prevTime = performance.now(), 
-    time = performance.now();
+    time = performance.now()
 let velocity = new THREE.Vector3();
-let y_rotation: number = 0;
+let y_rotation: number = 0
 let globalGroup = new THREE.Group();
 let moveUp: boolean = false, 
     moveDown: boolean = false, 
@@ -50,7 +50,16 @@ let geometry: THREE.PlaneGeometry,
     cloudMat: THREE.Material,
     boatMat: THREE.Material,
     boatMesh: THREE.Mesh,
-    boatP: THREE.Plane
+    boatP: THREE.Plane,
+    smokeParticles: THREE.InstancedMesh,
+    dummy: THREE.Object3D = new THREE.Object3D(),
+    dummyMat: THREE.Matrix4 = new THREE.Matrix4(),
+    dummyPos: THREE.Vector3 = new THREE.Vector3()
+const pOptions = {
+    count: 100,
+    maxHeight: 10,
+    spawnheight: 1
+}
 const boatv0 = new THREE.Vector3(0, 0, 0), boatv1 = new THREE.Vector3(0, 0, 0), boatv2 = new THREE.Vector3(0, 0, 0);
 const boatInd: number[] = [560, 561, 595]
 const noise = SimplexNoise.createNoise2D();
@@ -87,7 +96,7 @@ function init() {
 
     composer = new EffectComposer( renderer )
     composer.addPass( new RenderPass( scene, camera ) )
-    composer.addPass( new RenderPixelatedPass( renderResolution, scene, camera ) )
+    // composer.addPass( new RenderPixelatedPass( renderResolution, scene, camera ) )
     let bloomPass = new UnrealBloomPass( screenResolution, .4, .1, .9 )
     composer.addPass( bloomPass )
 
@@ -179,13 +188,12 @@ function init() {
             cloudMesh = new THREE.InstancedMesh(cloud.geometry, cloud.material, cloudAmt);
             cloudMesh.instanceMatrix.needsUpdate = true;
             cloudMesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
-            let matrix = new THREE.Matrix4();
             for (let i = 0; i < cloudAmt; i++) {
                 let tmp_x = Math.floor(Math.random() * 400), tmp_y = Math.floor(Math.random() * 50-25);
                 let position = new THREE.Vector3(tmp_x, tmp_y, 10);
                 cloudPos.push(position);
-                matrix.setPosition(position);
-                cloudMesh.setMatrixAt(i, matrix);
+                dummyMat.setPosition(position);
+                cloudMesh.setMatrixAt(i, dummyMat);
             }
             cloudMesh.rotation.x = -Math.PI / 2;
             cloudMesh.rotation.z = 15*Math.PI/11;
@@ -220,6 +228,20 @@ function init() {
             boatP = createPlane(boatv0, boatv1, boatv2);
         });
     
+    }
+
+    {
+        const geometry = new THREE.SphereGeometry(0.5, 8, 8);
+        const material = new THREE.MeshBasicMaterial({ color: 0x555555, transparent: true, opacity: 0.5 });
+        smokeParticles = new THREE.InstancedMesh(geometry, material, pOptions.count);
+        globalGroup.add(smokeParticles);
+
+        // Initialize particles
+        for (let i = 0; i < pOptions.count; i++) {
+            resetParticle(i);
+        }
+        smokeParticles.instanceMatrix.needsUpdate = true;
+        smokeParticles.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
     }
 
     // Lights
@@ -283,16 +305,41 @@ function updateClouds(delta: number) {
         if (cloudMesh.visible) cloudMesh.instanceMatrix.needsUpdate = true;
     }
 }
+function resetParticle(index: number) {
+    console.log(index)
+    dummy.position.set(
+        (Math.random() - 0.5) * 20,  // X position
+        pOptions.spawnheight,                 // Y position
+        (Math.random() - 0.5) * 20
+    );
+    dummy.updateMatrix();
+    smokeParticles.setMatrixAt(index, dummy.matrix);
+    smokeParticles.instanceMatrix.needsUpdate = true;
+}
+function updateSmoke() {
+    for (let i = 0; i < pOptions.count; i++) {
+        let matrix = new THREE.Matrix4();
+        smokeParticles.getMatrixAt(i, matrix);
+        dummy.matrix.copy(matrix);
+        // dummy.position.y += 0.001;
+        // dummy.position.x += (Math.random() - 0.5) * 0.01;
+        // dummy.position.z += (Math.random() - 0.5) * 0.01;
+
+        if (dummy.position.y > pOptions.maxHeight) {
+            resetParticle(i);
+        } else {
+            dummy.updateMatrix();
+            smokeParticles.setMatrixAt(i, dummy.matrix);
+        }
+    }
+
+    smokeParticles.instanceMatrix.needsUpdate = true;
+}
 
 function projPlane(point: THREE.Vector3, plane: THREE.Plane) {
     const projectedPoint = new THREE.Vector3();
     plane.projectPoint(point, projectedPoint);
     return projectedPoint;
-}
-function updateBoatPos(plane: THREE.Plane) {
-    const boatPosition = boatMesh.position.clone();
-    const projectedPoint = projPlane(boatPosition, plane);
-    boatMesh.position.copy(projectedPoint);
 }
 function updatePlane(v0: THREE.Vector3, v1:THREE.Vector3, v2:THREE.Vector3) {
     const edge1 = new THREE.Vector3().subVectors(v1, v0);
@@ -313,10 +360,6 @@ function oscillateValue(min:number, max:number, frequency:number, time:number) {
     const offset = min + amplitude;
     return amplitude * Math.sin(frequency * time) + offset;
 }
-function boatRot(time: number) {
-    const angle = oscillateValue(-0.2,0.2,0.75,time/2000);
-    boatMesh.rotation.z = angle
-}
 
 function updateBoat(time: number) {
     if (boatMesh) {
@@ -324,8 +367,11 @@ function updateBoat(time: number) {
         const zs = [pos.getZ(594), pos.getZ(595), pos.getZ(561)]
         boatv0.setY(zs[0]), boatv1.setY(zs[1]), boatv2.setY(zs[2])
         updatePlane(boatv0, boatv1, boatv2);
-        updateBoatPos(boatP);
-        boatRot(time)
+        const boatPosition = boatMesh.position.clone();
+        const projectedPoint = projPlane(boatPosition, boatP);
+        boatMesh.position.copy(projectedPoint);
+        const angle = oscillateValue(-0.2,0.2,1,time/2000);
+        boatMesh.rotation.z = angle
     }   
 }
 
@@ -438,6 +484,7 @@ function animate() {
     updateClouds(delta);
     updateBoat(time);
     updateOcean();
+    updateSmoke();
 
 
     // let mat = ( crystalMesh.material as THREE.MeshPhongMaterial )
